5acbd90335c3901975df887b06607d34aad2545d
ibrahimAlikilic <ibrahim.alikilic@gmail.com>
2024-12-24 21:47:33 +0300

    AI ksımı için denemelere devam ediyorum indiirilmesi gereken paketler indirildi , denemelere devam ediliyor

--- WEB_PROGRAMLAMA_PROJESI_2024/Controllers/AIController.cs
+++ WEB_PROGRAMLAMA_PROJESI_2024/Controllers/AIController.cs
@@ -28,18 +28,18 @@ namespace WEB_PROGRAMLAMA_PROJESI_2024.Controllers
                 }
 
                 // API İsteği
-                var options = new RestClientOptions("https://www.ailabapi.com")
+                var options = new RestClientOptions("https://www.ailabapi.com/api/portrait/effects/hairstyle-editor") //https://www.ailabapi.com
                 {
                     MaxTimeout = -1,
                 };
                 var client = new RestClient(options);
                 var request = new RestRequest("/api/portrait/effects/hairstyle-editor", Method.Post);
-                request.AddHeader("ailabapi-api-key", "dovAs5NhpGBirsFSlI5RU0SMMYqPfvy1VXkKnr7wHJiDYtWjB2aoE87ZbgOtceCe");
+                request.AddHeader("ailabapi-api-key", "kbMFSNOLT94DOG7F05knPEmxImXhNatY2RLtTJeosMUHwzUPcjvQj1GSf3HAdfhn");
                 request.AlwaysMultipartFormData = true;
                 request.AddFile("image_target", tempFilePath);
                 request.AddParameter("hair_type", hairStyle);
 
-                var response = await client.ExecuteAsync(request);   
+                var response = await client.ExecuteAsync(request);
 
                 if (response.IsSuccessful)
                 {
